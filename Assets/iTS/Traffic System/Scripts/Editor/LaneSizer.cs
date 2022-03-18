17668a23535ef07a1085fe3b4a75be1dcc512023
Hadid <blunty.hadid@gmail.com>
2022-03-19 03:57:00 +0500

    its Commit

---
+++ Assets/iTS/Traffic System/Scripts/Editor/LaneSizer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(iTSSizer))]
+public class LaneSizer : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        iTSSizer sizer = (iTSSizer)this.target;
+
+        if(GUILayout.Button("Resize Lanes"))
+        {
+            sizer.SizeLanes();
+        }
+
+        if(GUILayout.Button("Init"))
+        {
+            sizer.Start();
+        }
+
+        if(GUILayout.Button("AddOffset"))
+        {
+            sizer.AddOffsetToNewLanes();
+        }
+    }
+}
---
+++ Assets/iTS/Traffic System/Scripts/Main/iTSSizer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iTSSizer : MonoBehaviour
+{
+    public TSMainManager tsManager;
+    public float laneSize = 1f;
+
+    [SerializeField]
+    private TSLaneInfo[] lanes;
+
+    [SerializeField]
+    private TSLaneInfo[] newLanes;
+
+    public Vector3 offsetToLanes = new Vector3(0f, 0f, 0f);
+
+    public void Start()
+    {
+        this.lanes = this.tsManager.lanes;
+    }
+
+    public void SizeLanes()
+    {
+        if (this.lanes == null)
+            this.Start();
+
+        for(int i=0;i<this.lanes.Length;i++)
+        {
+            this.lanes[i].laneWidth = this.laneSize;
+        }
+    }
+
+    public void AddOffsetToNewLanes()
+    {
+        for(int i=0;i<this.tsManager.lanes.Length;i++)
+        {
+            this.lanes[i].conectorA += this.offsetToLanes;
+            this.lanes[i].conectorB += this.offsetToLanes;
+        }
+    }
+}
