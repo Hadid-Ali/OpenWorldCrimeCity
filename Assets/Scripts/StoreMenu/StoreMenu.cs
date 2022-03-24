8d1ef32648a3148c652fae399946a7e9f89fc99e
AaronGullraiz <aarongullraiz@gmail.com>
2022-03-24 21:47:48 +0500

    Store Updated

--- Assets/Scripts/StoreMenu/StoreMenu.cs
+++ Assets/Scripts/StoreMenu/StoreMenu.cs
@@ -11,6 +11,8 @@ public class StoreMenu : MonoBehaviour
     public Transform[] storeItemLists;
     public Transform storeItemButton;
 
+    private List<GameObject> buttonsList;
+
     #region Instance
     public static StoreMenu Instance;
     private void Awake()
@@ -29,11 +31,61 @@ public class StoreMenu : MonoBehaviour
 
     private void InitStore()
     {
+        buttonsList = new List<GameObject>();
+        buttonsList.Add(storeItemButton.gameObject);
         SetStoreButton(storeItemButton, 0);
         for (int i = 1; i < storeItemsDataList.Length; i++)
         {
-            var btn = Instantiate(storeItemButton.gameObject, storeItemLists[storeItemsDataList[i].listNo]) as GameObject;
-            SetStoreButton(btn.transform, i);
+            buttonsList.Add(Instantiate(storeItemButton.gameObject, storeItemLists[storeItemsDataList[i].listNo]) as GameObject);
+            SetStoreButton(buttonsList[i].transform, i);
+        }
+    }
+
+    private void SetStoreButton(Transform button, int index)
+    {
+        StoreItemData data = storeItemsDataList[index];
+        button.transform.name = data.itemName + index;
+        var btn = button.GetComponent<Button>();
+        btn.onClick.RemoveAllListeners();
+        btn.onClick.AddListener(() => ButtonClickEvent(index));
+        button.GetChild(0).GetComponent<Text>().text = data.itemName;
+        button.GetChild(1).GetComponent<Image>().sprite = data.icon;
+        if (data.isLocked && PreferenceManager.GetWeaponLockedStatus(data.itemName))
+        {
+            button.GetChild(2).gameObject.SetActive(true);
+            button.GetChild(3).gameObject.SetActive(true);
+            switch (data.unlockItemBy)
+            {
+                case UnlockItemType.RAD:
+                {
+                    button.GetChild(2).GetChild(0).gameObject.SetActive(true);
+                    button.GetChild(2).GetChild(1).gameObject.SetActive(false);
+                    button.GetChild(2).GetChild(2).gameObject.SetActive(false);
+                    button.GetChild(2).GetChild(0).GetChild(0).GetComponent<Text>().text = "X " + PreferenceManager.GetWeaponRAdCount(data.itemName);
+                    break;
+                }
+                case UnlockItemType.CASH:
+                {
+                    button.GetChild(2).GetChild(0).gameObject.SetActive(false);
+                    button.GetChild(2).GetChild(1).gameObject.SetActive(false);
+                    button.GetChild(2).GetChild(2).gameObject.SetActive(true);
+                    button.GetChild(2).GetChild(2).GetChild(0).GetComponent<Text>().text = data.unlockItemAmountRequired.ToString();
+                    break;
+                }
+                case UnlockItemType.INAPPS:
+                {
+                    button.GetChild(2).GetChild(0).gameObject.SetActive(false);
+                    button.GetChild(2).GetChild(1).gameObject.SetActive(true);
+                    button.GetChild(2).GetChild(2).gameObject.SetActive(false);
+                    button.GetChild(2).GetChild(1).GetChild(0).GetComponent<Text>().text = data.itemPrice_forInApp;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            button.GetChild(2).gameObject.SetActive(false);
+            button.GetChild(3).gameObject.SetActive(false);
         }
     }
 
@@ -53,54 +105,49 @@ public class StoreMenu : MonoBehaviour
         }
     }
 
-    private void SetStoreButton(Transform button, int index)
+    public void ShowList(int index)
     {
-        StoreItemData data = storeItemsDataList[index];
-        button.transform.name = data.itemName + index;
-        var btn = button.GetComponent<Button>();
-        btn.onClick.RemoveAllListeners();
-        btn.onClick.AddListener(()=>ButtonClickEvent(index));
-        button.GetChild(0).GetComponent<Text>().text = data.itemName;
-        button.GetChild(1).GetComponent<Image>().sprite = data.icon;
-        if(data.isLocked && PreferenceManager.GetWeaponLockedStatus(data.itemName))
+        if (index < storeItemLists.Length)
         {
-            button.GetChild(2).gameObject.SetActive(true);
-            button.GetChild(3).gameObject.SetActive(true);
-            if (data.getFromRad)
+            for (int i = 0; i < storeItemLists.Length; i++)
             {
-                button.GetChild(2).GetChild(0).gameObject.SetActive(true);
-                button.GetChild(2).GetChild(1).gameObject.SetActive(false);
-                button.GetChild(2).GetChild(0).GetChild(0).GetComponent<Text>().text = "X " + PreferenceManager.GetWeaponRAdCount(data.itemName);
-            }
-            else
-            {
-                button.GetChild(2).GetChild(0).gameObject.SetActive(false);
-                button.GetChild(2).GetChild(1).gameObject.SetActive(true);
-                button.GetChild(2).GetChild(1).GetChild(0).GetComponent<Text>().text = data.itemPrice_forInApp;
+                if(i == index)
+                {
+                    storeItemLists[i].parent.gameObject.SetActive(true);
+                }
+                else
+                {
+                    storeItemLists[i].parent.gameObject.SetActive(false);
+                }
             }
         }
         else
         {
-            button.GetChild(2).gameObject.SetActive(false);
-            button.GetChild(3).gameObject.SetActive(false);
+            storeItemLists[0].parent.gameObject.SetActive(true);
         }
     }
 
     private void ButtonClickEvent(int storeItemIndex)
     {
         var storeItem = storeItemsDataList[storeItemIndex];
-        if (storeItem.isLocked)
+        if (storeItem.isLocked && PreferenceManager.GetWeaponLockedStatus(storeItem.itemName))
         {
             if (PreferenceManager.GetWeaponLockedStatus(storeItem.itemName))
             {
                 // Item is locked
-                if (storeItem.getFromRad)
+                switch (storeItem.unlockItemBy)
                 {
-
-                }
-                else
-                {
-
+                    case UnlockItemType.RAD:
+                        GetItemFromRad(storeItem,storeItemIndex);
+                        break;
+                    case UnlockItemType.CASH:
+                        GetItemFromCash(storeItem,storeItemIndex);
+                        break;
+                    case UnlockItemType.INAPPS:
+                        GetItemFromInApps(storeItem, storeItemIndex);
+                        break;
+                    default:
+                        break;
                 }
             }
             else
@@ -108,6 +155,59 @@ public class StoreMenu : MonoBehaviour
                 // Item is unlocked
             }
         }
+    }
+
+    public void GetItemFromRad(StoreItemData storeItem,int index)
+    {
+#if UNITY_EDITOR
+        OnRadRewardUser(storeItem, index);
+#else
+        // Call rad show
+        // On reward user call 
+        // OnRadRewardUser()
+#endif
+    }
+
+    private void OnRadRewardUser(StoreItemData storeItem, int index)
+    {
+        PreferenceManager.SetWeaponLockedStatus(storeItem.itemName, PreferenceManager.GetWeaponRAdCount(storeItem.itemName)-1);
+        buttonsList[index].transform.GetChild(2).GetChild(0).GetChild(0).GetComponent<Text>().text = "X " + PreferenceManager.GetWeaponRAdCount(storeItem.itemName);
+        if (PreferenceManager.GetWeaponRAdCount(storeItem.itemName) <= 0)
+        {
+            UnlockItem(storeItem,index);
+        }
+    }
+
+    public void GetItemFromInApps(StoreItemData storeItem, int index)
+    {
+#if UNITY_EDITOR
+        UnlockItem(storeItem, index);
+#else
+        // Call IAP manager
+        // In-app bundle is stored in
+        // storeItem.inAppBundle
+#endif
+    }
+
+    public void GetItemFromCash(StoreItemData storeItem, int index)
+    {
+        if(PreferenceManager.CashBalance >= storeItem.unlockItemAmountRequired)
+        {
+            PreferenceManager.CashBalance -= storeItem.unlockItemAmountRequired;
+            UnlockItem(storeItem, index);
+        }
+        else
+        {
+            // Show not enough coins
+            Debug.LogError("Not Enough Cash");
+        }
+    }
+
+    private void UnlockItem(StoreItemData storeItem, int index)
+    {
+        buttonsList[index].transform.GetChild(2).gameObject.SetActive(false);
+        buttonsList[index].transform.GetChild(3).gameObject.SetActive(false);
+        PreferenceManager.SetWeaponLockedStatus(storeItem.itemName);
     }
 }
 
@@ -117,8 +217,14 @@ public class StoreItemData
     public string itemName = "";
     public int listNo = 0;
     public Sprite icon;
-    public bool isLocked, getFromRad = false;
-    public int rAdCount = 2;
+    public bool isLocked;
+    public UnlockItemType unlockItemBy = UnlockItemType.NONE;
+    public int unlockItemAmountRequired = 2;
     public string inAppBundle = "";
     public string itemPrice_forInApp = "$";
+}
+
+public enum UnlockItemType
+{
+    NONE,RAD,CASH,INAPPS,
 }
