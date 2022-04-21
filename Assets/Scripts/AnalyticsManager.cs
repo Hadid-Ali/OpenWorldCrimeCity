2efc5ae51321d48946a9e52419ababad20344ec4
Hadid <blunty.hadid@gmail.com>
2022-04-22 02:36:20 +0500

    Changes Commit

---
+++ Assets/Scripts/AnalyticsManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnalyticsManager : MonoBehaviour
+{
+    public static void DesignEvent(string prefix,string eventData)
+    {
+        AppMetrica.Instance.ReportEvent(eventData);
+    }
+      
+}
